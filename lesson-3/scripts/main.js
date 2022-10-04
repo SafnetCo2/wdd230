741de5234cdd48b953bde1db09ee281a0defc678
J2coding <kimmyjmueni@gmail.com>
2022-10-04 18:25:37 +0300

    modify

--- lesson-3/scripts/main.js
+++
@@ -1,8 +0,0 @@
-//get date and time 
-
-var today = new Date();
-var date=today.getDate()+"/"+(today.getMonth()+1)+"/"+today.getFullYear();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' '+ time;
-
-document.getElementById("date-time").innerHTML=dateTime;
