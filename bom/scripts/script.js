9c5762c0b955c598b7980fe60adb239e3b0f0031
Josephine Mueni <nzi1900@byui.edu>
2022-10-14 08:45:36 -0700

    favicon icon

--- bom/scripts/script.js
+++ bom/scripts/script.js
@@ -1,26 +1,47 @@
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
 
-      const list = document.querySelector('ul');
-      const input = document.querySelector('input');
-      const button = document.querySelector('button');
+button.addEventListener('click', () => {
+  console.log(input.value)
 
-      button.addEventListener('click', () => {
-        const myItem = input.value;
-        input.value = '';
+  // const addChapter = input.value;
+  //If the input value is not empty then do:
+  if(input.value != []){
+    //Create a li element
 
-        const listItem = document.createElement('li');
-        const listText = document.createElement('span');
-        const listBtn = document.createElement('button');
+    //Create a button
 
-        listItem.appendChild(listText);
-        listText.textContent = myItem;
-        listItem.appendChild(listBtn);
-        listBtn.textContent = 'X';
-        list.appendChild(listItem);
+    //Update the list item to have the input value
 
-        listBtn.addEventListener('click', () => {
-          list.removeChild(listItem);
-        });
+    //Update the button to have an X
 
-        input.focus();
-      });
+    //Append the button to the li
+
+    //Append the li to the list
+
+    //Add an event listener to the delete btn with deletes the li from the list 
+
+  }
+    
+
+
+
+// const listcontainer = document.createElement('li');
+// // const listText = document.createElement('span');
+// const listBtn = document.createElement('button');
+
+// // listcontainer.appendChild(listText);
+// listText.textContent = addChapter;
+// listcontainer.appendChild(listBtn);
+// listBtn.textContent = 'X';
+// list.appendChild(listcontainer);
+
+// listBtn.addEventListener('click', () => {
+//   list.removeChild(listcontainer);
+// });
+
+// input.focus();
+
+});
     
