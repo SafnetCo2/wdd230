8ad76e936535fc4648d78bcd5524a3543f759230
Josephine Mueni <nzi1900@byui.edu>
2022-10-23 12:02:26 -0700

    modify file with srcset images and icons

---
+++ weather-chamber/New folder/scripts/scripts.js
@@ -0,0 +1,37 @@
+function toggleMenu(){
+  document.getElementById("primaryN").classList.toggle("open")
+  document.getElementById("btn").classList.toggle("open")
+}
+const x =document.getElementById("btn")
+x.onclick=toggleMenu;
+
+// Date
+// const d= new Date();
+// document.getElementById("date-time").innerHTML=d;
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+// const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+// const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+// 	dateStyle: "full"}).format(now);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+// datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+// const dt=new Date();
+// document.getElementById("date-time").innerHTML=dt+"/";
+
+// const hero = new Date();
+// const  day=hero.getDay();
+// if (day < 1 || day > 2) {
+//     const x = document.querySelector('.dateT');
+//     x.remove();
+//   }
+  document.querySelector(
+	"#Last-updated"
+).textContent = `Last-updated: ${document.lastModified}`;
