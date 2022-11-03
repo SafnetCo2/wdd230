c3c819507c648345cd480574d4153cf95fa1e007
Josephine Mueni <nzi1900@byui.edu>
2022-11-03 08:55:50 -0700

    delete asset and discover folder

--- lesson-8b/fake/scripts/scripts.js
+++ lesson-8b/fake/scripts/scripts.js
@@ -33,38 +33,6 @@ if (day < 1 || day > 2) {
     x.remove();
   }
 
-  let options={
-    root:null,
-    rootMargin: "0px",
-    threshold:0.5
-    }
-    //create call back function
-    let callback=(entries,observer) =>{
-        entries.forEach(entry => {
-            if(entry.isIntersecting && entry.target.className==="image"){
-  
-                let imageUrl =entry.target.getAttribute("data-src");
-                if(imageUrl){
-                    entry.target.src=imageUrl;
-                    observer.unobserver(entry.target);
-                }
-            }
-            
-        });
-        
-    }
-  let observer = new IntersectionObserver(callback,options)
-  observer.observe(document.querySelector("#image1"));
-  observer.observe(document.querySelector("#image2"));
-  observer.observe(document.querySelector("#image3"))
-  observer.observe(document.querySelector("#image4"))
-  observer.observe(document.querySelector("#image5"))
-  observer.observe(document.querySelector("#image6"))
-
-
-
-
-
 //windchill
 //input: get the temperature and speed
 //process: compute for windchill factor
